86189929de4d760317015a6934ba6bf107ba11be
Juan Soto <jsotcast@nttdata.com>
2021-12-12 21:56:58 -0500

    refactor add configuration config, eureka and gateway

---
+++ transfer-service/src/main/java/com/nttdata/transfer/TransferServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class TransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferServiceApplication.class, args);
+	}
+
+}
